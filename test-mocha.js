f46eea576c27344e14042cd6b72bc2d695659105
renatoac <renatoac82@gmail.com>
2012-05-09 14:11:24 -0300

    Update test-mocha.js

--- test-mocha.js
+++ test-mocha.js
@@ -2,9 +2,9 @@ var should = require('should'),
 	rest = require('restler');
 
 describe('api.staging', function(){
-  describe('GET catalog/v1.1/titles/', function(){
+  describe('GET http://www.example.com', function(){
     it('respond with status code', function(callback){
-		rest.get('http://api.staging.netmovies.com.br/catalog/v1.1/titles/')
+		rest.get('http://www.example.com')
 			.on('complete', function (data, response) {
 			  response.statusCode.should.equal(200);
 			  callback();
